6dd1d3b550c21d959fb233c00d062706887209a3
Jun Tamaru <jst223@gmail.com>
2021-12-31 17:26:08 +0900

    Modified so that the text be extracted.

--- bibleRead.py
+++ bibleRead.py
@@ -5,42 +5,50 @@ import re
 
 from bs4 import BeautifulSoup
 
-def today_string():
+
+def today_str():
+
     today = datetime.date.today()
     if today.month > 9:
-        today_string_out = str(today.month)
+        str_out = str(today.month)
     else:
-        today_string_out = "0"+str(today.month)
+        str_out = "0" + str(today.month)
     if today.day > 9:
-        today_string_out = today_string_out + str(today.day)
+        str_out = str_out + str(today.day)
     else:
-        today_string_out = today_string_out + "0" + str(today.day)
-    year_string = str(today.year)
-    today_string_out = today_string_out + year_string[2] + year_string[3]
-    return today_string_out
+        str_out = str_out + "0" + str(today.day)
+    year_str = str(today.year)
+    str_out = str_out + year_str[2] + year_str[3]
+    return str_out
 
-def print_today_bible(daystring):
-    bible_reading_url='http://www.usccb.org/bible/readings/' + daystring + '.cfm'
 
-    r = requests.get(bible_reading_url)
+def p_today_bbl(day_str):
+
+    usccb_url = 'http://www.usccb.org/bible/readings/'
+    bbl_url = usccb_url + day_str + '.cfm'
+    print(bbl_url)
+    r = requests.get(bbl_url)
 
     html = r.text
+#    print(html)
     soup = BeautifulSoup(html, "html.parser")
 
-    for each in soup.find_all(class_="bibleReadingsWrapper"):
-        if len(str(each.parent))>50:
+    for each in soup.select(".content-body, .content-header > h3"):
+#        print(each)
+        if len(str(each.parent)) > 50:
             out_text = re.sub(r'</em>|<em>|</br>|<strong>|</strong>|<div>|</div>|<h4>|</h4>|</a>|<br/>|<div¥ class=¥"bibleReadingsWrapper¥">|<div class=¥"¥">', " ", str(each.text))
             out_text2 = re.sub(r'<br>', ' ', out_text)
             print(str(out_text2))
         else:
             return "TWO"
     return "OK"
-        
-print (datetime.date.today())
-rtn = print_today_bible(today_string())
 
-if rtn=="TWO": # for the feast of Assumption etc ?
+
+print(datetime.date.today())
+rtn = p_today_bbl(today_str())
+
+if rtn == "TWO":  # for the feast of Assumption etc ?
     print("Vigil Mass:")
-    print_today_bible(today_string()+"-vigil")
+    p_today_bbl(today_str() + "-vigil")
     print("Day Mass:")
-    print_today_bible(today_string()+"-day-mass")
+    p_today_bbl(today_str() + "-day-mass")
